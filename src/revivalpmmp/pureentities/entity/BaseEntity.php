7b878ef8d1752d4cecd04f053f25358afc26695b
iZeaoGamer <jameshayward14@icloud.com>
2018-05-19 17:05:58 +0100

    Update BaseEntity.php

--- src/revivalpmmp/pureentities/entity/BaseEntity.php
+++ src/revivalpmmp/pureentities/entity/BaseEntity.php
@@ -189,7 +189,7 @@ abstract class BaseEntity extends Creature{
 		return $this->maxJumpHeight;
 	}
 
-	public function initEntity(){
+	public function initEntity(): void{
 		parent::initEntity();
 
 		$this->loadNBT();
