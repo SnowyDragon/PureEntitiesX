7ec2350e94f9475d75cac4de53272129a37d88c7
iZeaoGamer <jameshayward14@icloud.com>
2018-05-19 17:45:32 +0100

    Update BaseEntity.php

--- src/revivalpmmp/pureentities/entity/BaseEntity.php
+++ src/revivalpmmp/pureentities/entity/BaseEntity.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * PureEntitiesX: Mob AI Plugin for PMMP
  * Copyright (C)  2018 RevivalPMMP
@@ -17,9 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 namespace revivalpmmp\pureentities\entity;
-
 use pocketmine\block\Block;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\AddEntityPacket;
@@ -41,21 +38,16 @@ use revivalpmmp\pureentities\features\IntfCanPanic;
 use revivalpmmp\pureentities\features\IntfTameable;
 use revivalpmmp\pureentities\PluginConfiguration;
 use revivalpmmp\pureentities\PureEntities;
-
 abstract class BaseEntity extends Creature{
-
 	protected $networkId = null;
 	public $stayTime = 0;
 	protected $moveTime = 0;
-
 	/** @var Vector3|Entity */
 	private $baseTarget = null;
-
 	private $movement = true;
 	protected $friendly = false;
 	private $wallcheck = true;
 	protected $fireProof = false;
-
 	/**
 	 * Default is 1.2 blocks because entities need to be able to jump
 	 * just higher than the block to land on top of it.
@@ -69,24 +61,17 @@ abstract class BaseEntity extends Creature{
 	public $width = 1.0;
 	public $height = 1.0;
 	public $speed = 1.0;
-
-
 	/**
 	 * @var int
 	 */
 	private $checkTargetSkipCounter = 0;
-
 	/**
 	 * @var IdlingComponent
 	 */
 	protected $idlingComponent;
-
-
 	protected $maxAge = 0;
-
 	public function __destruct(){
 	}
-
 	public function __construct(Level $level, CompoundTag $nbt){
 		$this->idlingComponent = new IdlingComponent($this);
 		$this->checkTargetSkipTicks = PluginConfiguration::getInstance()->getCheckTargetSkipTicks();
@@ -100,9 +85,7 @@ abstract class BaseEntity extends Creature{
 			$this->namedtag->setByte("generatedByPEX", 1);
 		}
 	}
-
 	public abstract function updateMove($tickDiff);
-
 	/**
 	 * Should return the experience dropped by the entity when killed
 	 * @return int
@@ -110,40 +93,31 @@ abstract class BaseEntity extends Creature{
 	public function getXpDropAmount() : int{
 		return 0; // default no experience drops
 	}
-
 	public function getSaveId(): string{
 		$class = new \ReflectionClass(get_class($this));
 		return $class->getShortName();
 	}
-
 	public function isMovement() : bool{
 		return $this->movement;
 	}
-
 	public function isFriendly() : bool{
 		return $this->friendly;
 	}
-
 	public function isKnockback() : bool{
 		return $this->attackTime > 0;
 	}
-
 	public function isWallCheck() : bool{
 		return $this->wallcheck;
 	}
-
 	public function setMovement(bool $value){
 		$this->movement = $value;
 	}
-
 	public function setFriendly(bool $bool){
 		$this->friendly = $bool;
 	}
-
 	public function setWallCheck(bool $value){
 		$this->wallcheck = $value;
 	}
-
 	/**
 	 * Sets the base target for the entity. If this method is called
 	 * and the baseTarget is the same, nothing is set
@@ -156,19 +130,15 @@ abstract class BaseEntity extends Creature{
 			$this->baseTarget = $baseTarget;
 		}
 	}
-
 	public function setNetworkId(int $networkId){
 		$this->networkId = $networkId;
 	}
-
-
 	/*
 	 * Returns the appropriate Network Id for this entity.
 	 */
 	public function getNetworkId(){
 		return $this->networkId;
 	}
-
 	/**
 	 * Returns the base target currently set for this entity
 	 *
@@ -177,56 +147,44 @@ abstract class BaseEntity extends Creature{
 	public function getBaseTarget(){
 		return $this->baseTarget;
 	}
-
 	public function getSpeed() : float{
 		return $this->speed;
 	}
-
 	/**
 	 * @return int
 	 */
 	public function getMaxJumpHeight() : int{
 		return $this->maxJumpHeight;
 	}
-
 	public function initEntity(): void{
 		parent::initEntity();
-
 		$this->loadNBT();
-
 		$this->setDataFlag(self::DATA_FLAG_NO_AI, self::DATA_TYPE_BYTE, 1);
-
 		$this->idlingComponent->loadFromNBT();
 	}
-
 	public function saveNBT(): void{
 		if(PluginConfiguration::getInstance()->getEnableNBT()){
 			parent::saveNBT();
 			$this->namedtag->setByte(NBTConst::NBT_KEY_MOVEMENT, $this->isMovement());
 			$this->namedtag->setByte(NBTConst::NBT_KEY_WALL_CHECK, $this->isWallCheck());
 			$this->namedtag->setInt(NBTConst::NBT_KEY_AGE_IN_TICKS, $this->age);
-
 			// No reason to attempt this if getEnableNBT is false.
 			$this->idlingComponent->saveNBT();
 		}
 	}
-
 	public function loadNBT(): void{
 		if(PluginConfiguration::getInstance()->getEnableNBT()){
 			if(($movement = $this->namedtag->getByte(NBTConst::NBT_KEY_MOVEMENT, NBTConst::NBT_INVALID_BYTE)) != NBTConst::NBT_INVALID_BYTE){
 				$this->setMovement(boolval($movement));
 			}
-
 			if(($wallCheck = $this->namedtag->getByte(NBTConst::NBT_KEY_WALL_CHECK, NBTConst::NBT_INVALID_BYTE)) != NBTConst::NBT_INVALID_BYTE){
 				$this->setWallCheck(boolval($wallCheck));
 			}
-
 			if(($age = $this->namedtag->getInt(NBTConst::NBT_KEY_AGE_IN_TICKS, NBTConst::NBT_INVALID_INT)) != NBTConst::NBT_INVALID_INT){
 				$this->age = $age;
 			}
 		}
 	}
-
 	public function spawnTo(Player $player): void{
 		if(
 			!isset($this->hasSpawned[$player->getLoaderId()])
@@ -241,104 +199,80 @@ abstract class BaseEntity extends Creature{
 			$pk->pitch = $this->pitch;
 			$pk->metadata = $this->propertyManager->getAll();
 			$player->dataPacket($pk);
-
 			$this->hasSpawned[$player->getLoaderId()] = $player;
 		}
 	}
-
 	public function updateMovement(bool $teleport = false): void{
 		if(!$this->isClosed() && $this->getLevel() !== null){
 			parent::updateMovement();
 		}
 	}
-
 	public function isInsideOfSolid() : bool{
 		$block = $this->level->getBlock($this->temporalVector->setComponents(Math::floorFloat($this->x), Math::floorFloat($this->y + $this->height - 0.18), Math::floorFloat($this->z)));
 		$bb = $block->getBoundingBox();
 		return $bb !== null and $block->isSolid() and !$block->isTransparent() and $bb->intersectsWith($this->getBoundingBox());
 	}
-
 	/**
 	 * Entity gets attacked by another entity / explosion or something similar
 	 *
 	 * @param EntityDamageEvent $source the damage event
 	 */
 	public function attack(EntityDamageEvent $source): void{
-
 		if($this->isClosed() || $source->isCancelled() || !($source instanceof EntityDamageByEntityEvent)){
 			return;
 		}
-
 		if($this->isKnockback() > 0) return;
-
 		// "wake up" entity - it gets attacked!
 		$this->idlingComponent->stopIdling(1, true);
-
 		parent::attack($source);
-
 		$this->stayTime = 0;
 		$this->moveTime = 0;
-
 		$sourceOfDamage = $source->getDamager();
 		$motion = (new Vector3($this->x - $sourceOfDamage->x, $this->y - $sourceOfDamage->y, $this->z - $sourceOfDamage->z))->normalize();
 		$this->motionX = $motion->x * 0.19;
 		$this->motionZ = $motion->z * 0.19;
-
 		if(($this instanceof FlyingEntity) && !($this instanceof Blaze)){
 			$this->motionY = $motion->y * 0.19;
 		}else{
 			$this->motionY = 0.6;
 		}
-
 		// panic mode - here we check if the entity can enter panic mode and so on
 		if($this instanceof IntfCanPanic and $sourceOfDamage instanceof Player and !$this->isInPanic() and $this->panicEnabled()){
 			$this->setBaseTarget(new Vector3($this->x - ($sourceOfDamage->x * 10), $this->y - $sourceOfDamage->y, ($this->z - $sourceOfDamage->z * 10)));
 			$this->setInPanic(); // this should prevent to search for other targets and increase run speed
 		}
-
 		$this->checkAttackByTamedEntities($source);
 	}
-
 	public function knockBack(Entity $attacker, float $damage, float $x, float $z, float $base = 0.4): void{
-
 	}
-
 	public function entityBaseTick(int $tickDiff = 1) : bool{
 		Timings::$timerEntityBaseTick->startTiming();
 		// check if it needs to despawn
-
 		$hasUpdate = Entity::entityBaseTick($tickDiff);
-
 		// Checking this first because there's no reason to keep going if we know
 		// we're going to despawn the entity.
 		if($this->checkDespawn()){
 			Timings::$timerEntityBaseTick->stopTiming();
 			return false;
 		}
-
 		if($this->isInsideOfSolid()){
 			$hasUpdate = true;
 			$ev = new EntityDamageEvent($this, EntityDamageEvent::CAUSE_SUFFOCATION, 1);
 			$this->attack($ev);
 		}
-
 		if($this->moveTime > 0){
 			$this->moveTime -= $tickDiff;
 		}
-
 		if($this->attackTime > 0){
 			$this->attackTime -= $tickDiff;
 		}
-
 		// check panic tick
 		if($this instanceof IntfCanPanic){
 			$this->panicTick($tickDiff);
 		}
-
 		Timings::$timerEntityBaseTick->stopTiming();
 		return $hasUpdate;
 	}
-
 	/**
 	 * This method checks if an entity should despawn - if so, the entity is closed
 	 * @return bool
@@ -354,21 +288,17 @@ abstract class BaseEntity extends Creature{
 		}
 		return false;
 	}
-
-	public function move(float $dx, float $dy, float $dz) : void{
+	public function move(float $dx, float $dy, float $dz): void{
 		Timings::$entityMoveTimer->startTiming();
-
 		$movX = $dx;
 		$movY = $dy;
 		$movZ = $dz;
-
 		$list = $this->level->getCollisionCubes($this, $this->level->getTickRate() > 1 ? $this->boundingBox->getOffsetBoundingBox($dx, $dy, $dz) : $this->boundingBox->addCoord($dx, $dy, $dz));
 		if($this->isWallCheck()){
 			foreach($list as $bb){
 				$dx = $bb->calculateXOffset($this->boundingBox, $dx);
 			}
 			$this->boundingBox->offset($dx, 0, 0);
-
 			foreach($list as $bb){
 				$dz = $bb->calculateZOffset($this->boundingBox, $dz);
 			}
@@ -378,21 +308,15 @@ abstract class BaseEntity extends Creature{
 			$dy = $bb->calculateYOffset($this->boundingBox, $dy);
 		}
 		$this->boundingBox->offset(0, $dy, 0);
-
 		$this->setComponents($this->x + $dx, $this->y + $dy, $this->z + $dz);
 		$this->checkChunks();
-
 		$this->checkGroundState($movX, $movY, $movZ, $dx, $dy, $dz);
 		$this->updateFallState($dy, $this->onGround);
-
 		Timings::$entityMoveTimer->stopTiming();
-		return true;
 	}
-
 	public function targetOption(Creature $creature, float $distance) : bool{
 		return $this instanceof Monster && (!($creature instanceof Player) || ($creature->isSurvival() && $creature->spawned)) && $creature->isAlive() && !$creature->isClosed() && $distance <= 81;
 	}
-
 	/**
 	 * This is called while moving around. This is specially important for entities like sheep etc. pp
 	 * which eat grass to grow their wool. This method should check at which block the entity is currently
@@ -403,7 +327,6 @@ abstract class BaseEntity extends Creature{
 	public function isCurrentBlockOfInterest(){
 		return false;
 	}
-
 	/**
 	 * This method is used to determine if the attack comes from a player. When the player has tamed
 	 * entities - all will attack (when not already attacking another monster).
@@ -443,7 +366,6 @@ abstract class BaseEntity extends Creature{
 			}
 		}
 	}
-
 	/**
 	 * Call this function in any entity you want the tamed mobs to attack when player gets attacked.
 	 * Take a look at Zombie Entity for usage!
@@ -463,7 +385,6 @@ abstract class BaseEntity extends Creature{
 			}
 		}
 	}
-
 	/**
 	 * Returns all tamed mobs for the given player ...
 	 * @param Player $player
@@ -483,7 +404,6 @@ abstract class BaseEntity extends Creature{
 		}
 		return $tamedMobs;
 	}
-
 	/**
 	 * Checks if checkTarget can be called. If not, this method returns false
 	 *
@@ -498,7 +418,6 @@ abstract class BaseEntity extends Creature{
 			return false;
 		}
 	}
-
 	/**
 	 * Checks if dropping loot is allowed.
 	 * @return bool true when allowed, false when not
@@ -510,7 +429,6 @@ abstract class BaseEntity extends Creature{
 		}
 		return false;
 	}
-
 	/**
 	 * Checks if this entity is following a player
 	 *
